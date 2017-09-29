941fd74c48494adb934d5ef188594069698f1c1d
d1mbas <tambovcev.dmitry@restream.rt.ru>
2017-09-29 16:28:24 +0300

    release 0.0.7

--- most_common_words/__init__.py
+++ most_common_words/__init__.py
@@ -5,6 +5,6 @@ from .utils import flat
 from .nltk_downloader import NLTKDownloader, NLTKDownloaderError
 from .printer import Printer
 
-__version__ = '0.0.7-pre.2'
+__version__ = '0.0.7'
 
 __all__ = ('MostCommonWords', 'flat', 'NLTKDownloader', 'NLTKDownloaderError', 'Printer')
